62ec503c3fc7da8b11fac593b14413a04012f46b
Christoph Pilka <c.pilka@asconix.com>
2026-02-10 06:30:33 +0100

    feat: created a seperate Emacs config file instead of inline config

---
+++ home/modules/emacs/init.el
@@ -0,0 +1,18 @@
+;; --- Vanilla init.el managed by Home Manager ---
+
+(setq inhibit-startup-screen t
+    make-backup-files nil
+    auto-save-default nil
+    create-lockfiles nil
+    ring-bell-function 'ignore)
+
+    (tool-bar-mode -1)
+    (menu-bar-mode -1)
+    (scroll-bar-mode -1)
+
+    (global-display-line-numbers-mode 1)
+    (column-number-mode 1)
+
+    (when (eq system-type 'darwin)
+      (setq mac-option-modifier 'meta)
+      (setq mac-command-modifier 'super))
