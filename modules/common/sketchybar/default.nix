61dc130d418ad3f3a789fcf5201a155a9523bacc
Christoph Pilka <c.pilka@asconix.com>
2026-04-02 21:18:14 +0200

    feat(sketchybar): added clock plugin

--- modules/common/sketchybar/default.nix
+++ modules/common/sketchybar/default.nix
@@ -5,8 +5,19 @@
       source = ./sketchybarrc;
       executable = true;
     };
+
     ".config/sketchybar/colors.sh" = {
       source = ./colors.sh;
+    };
+
+    ".config/sketchybar/plugins/aerospace.sh" = {
+      source = ./plugins/aerospace.sh;
+      executable = true;
+    };
+
+    ".config/sketchybar/plugins/clock.sh" = {
+      source = ./plugins/clock.sh;
+      executable = true;
     };
   };
 }
